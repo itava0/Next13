da59dc6b121e55042f5764d8397b3a1ebdae28cc
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-04 11:47:22 -0400

    Genarate page static

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,6 @@
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPosts"
+import getAllUsers from "@/lib/getAllUsers";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 import { Metadata } from "next";
@@ -40,4 +41,13 @@ export default async function UserPage({ params: { userId } }: Params) {
           </Suspense>
       </>
   )
+}
+
+export async function generateStaticParams() {
+    const usersData: Promise<User[]> = getAllUsers();
+    const users = await usersData;
+
+    return users.map(user => (
+        {userId: user.id.toString()}
+    ))
 }
