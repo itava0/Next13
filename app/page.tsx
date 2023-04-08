a7382ff9c55a7e365d03c4d0f2d0fecbeb9b4a5d
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-08 13:57:41 -0400

    Created a Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <main className={styles.main}>
+    <main className={inter.className}>
       <h1>Hello World!</h1>
     <Link href="/about">About Page</Link>
     </main>
